bd860935f9805c147e55c4a622a1777be7570b31
NRIVALANT <nicolas.rivalant@ynov.com>
2024-02-21 10:02:47 +0100

    test deals

---
+++ src/app/extensions/DealsCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "@hubspot/ui-extensions";
+
+const DealsCard = () => {
+  return (
+    <div>
+      <Text>
+        This is the content of the Deals Card. You can customize it as needed.
+      </Text>
+    </div>
+  );
+};
+
+export default DealsCard;
